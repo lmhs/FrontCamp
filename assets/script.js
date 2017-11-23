70fa09ba1fee0613b33b6a8bda8fd086eda2062d
Maksim Labovich <maksim_labovich@epam.com>
2017-11-24 00:04:38 +0300

    Added reloading results section on changing news source, additional ES6 features

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,69 @@
+// arrow function
 (() => {
-  const API_KEY = 'bdbeeb170f8c47a2b97aa0f6252bfb90';
-  const NEWS_API_URL = 'https://newsapi.org/v2'
-  const SOURCES_URL = `${NEWS_API_URL}/sources?apiKey=${API_KEY}`;
-  let headlinesUrl = `${NEWS_API_URL}/top-headlines?sources=&apiKey=${API_KEY}`;
-  let select = document.getElementById('news-channels');
+  // let and const
+  let settings = {
+    apiKey : 'bdbeeb170f8c47a2b97aa0f6252bfb90'
+  };
 
-  fetch(SOURCES_URL).then((response) => {
+  // desctructuring assignment
+  const {
+    apiKey : API_KEY,
+    newsAPIURL : NEWS_API_URL = 'https://newsapi.org/v2',
+    params : params = {
+      sources : SOURCES_PARAM = 'sources=',
+      apiKey : API_KEY_PARAM = 'apiKey='
+    },
+    paths : paths = {
+      sources : SOURCES_PATH = '/sources',
+      headlines : HEADLINES_PATH = '/top-headlines'
+    }
+  } = settings;
+
+  // template literals
+  const sourcesURL = `${NEWS_API_URL}${SOURCES_PATH}?${API_KEY_PARAM}${API_KEY}`;
+  const headlinesURL = `${NEWS_API_URL}${HEADLINES_PATH}?${SOURCES_PARAM}&${API_KEY_PARAM}${API_KEY}`;
+  const select = document.getElementById('news-channels');
+  const newsResults = document.getElementById('news-results');
+
+  // fetch
+  fetch(sourcesURL).then((response) => {
     if (response.status !== 200) {
-      console.log(`${response.status}`);
+      console.log(response.status);
       return;
     }
     return response.json();
   }).then((data) => {
-    data.sources.forEach((item) => {
-      select.insertAdjacentHTML('beforeend', createListItem(item));
+    data.sources.forEach((source) => {
+      select.insertAdjacentHTML('beforeend', createListItem(source));
+    });
+  });
+
+  select.addEventListener('change', (event) => {
+    const selectedValue = event.target.value;
+    const newHeadlinesURL = [headlinesURL.slice(0, headlinesURL.indexOf(`&${API_KEY_PARAM}`)),
+      selectedValue,
+      headlinesURL.slice(headlinesURL.indexOf(`&${API_KEY_PARAM}`), headlinesURL.length)].join('');
+
+    fetch(newHeadlinesURL).then((response) => {
+      if (response.status !== 200) {
+        console.log(response.status);
+        return;
+      }
+      return response.json();
+    }).then((data) => {
+      newsResults.innerHTML = '';
+      // array for of
+      for (const article of data.articles) {
+        newsResults.insertAdjacentHTML('beforeend', createArticle(article.description));
+      }
     });
   });
 
   function createListItem(source) {
-    return `<option>${source.name}</option>`
+    return `<option value="${source.id}">${source.name}</option>`;
+  }
+
+  function createArticle(content) {
+    return `<article>${content}</article>`;
   }
 })();
