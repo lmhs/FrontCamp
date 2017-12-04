cce49a6a026634c29c32fd9ff41c41d2d35a365d
Maksim Labovich <maksim_labovich@epam.com>
2017-12-04 08:55:37 +0300

    Added bundler (webpack), added plugin babel plugin for extending builtin types

--- src/scripts/NewsRequest.js
+++ src/scripts/NewsRequest.js
@@ -3,11 +3,12 @@ export default class NewsRequest extends Request {
 
   constructor(url) {
     let apiKey = 'bdbeeb170f8c47a2b97aa0f6252bfb90';
+    const headers = new Headers({
+      'X-Api-Key' : apiKey
+    });
 
     super(url, {
-      headers : new Headers({
-        'X-Api-Key' : apiKey
-      })
+      headers
     });
   }
 }
